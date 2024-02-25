dd26e07ffdce4cb2066b2c21346baacd3e34b244
Miraj Asraf <mirajasraf786@gmail.com>
2024-02-25 10:57:00 +0530

    Implemented routes and controllers

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,18 @@
-import express from 'express';
-import dotenv from 'dotenv';
+import express from "express";
+import dotenv from "dotenv";
+import authRoutes from "./routes/auth.route.js";
 
 const app = express();
 
 dotenv.config();
 const PORT = process.env.PORT || 5000;
 
-app.get('/', (req, res) => {
-    // root route 
-    res.send('Server is running!');
+app.get("/", (req, res) => {
+  // root route
+  res.send("Server is running!");
 });
+
+// auth routes
+app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
