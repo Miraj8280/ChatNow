cb75cdec1cebf73e3057ff534b5e1bb4cfae2fd5
Miraj Asraf <mirajasraf786@gmail.com>
2024-02-25 16:51:28 +0530

    User model and signup controller created

--- backend/server.js
+++ backend/server.js
@@ -4,17 +4,21 @@ import authRoutes from "./routes/auth.route.js";
 import connectToMongoDB from "./database/connectToMongoDB.js";
 
 const app = express();
-
-dotenv.config();
 const PORT = process.env.PORT || 5000;
 
-app.get("/", (req, res) => {
-  // root route
-  res.send("Server is running!");
-});
+dotenv.config();
+
+// to parse the incoming requests with JSON payloads (from req.body)
+app.use(express.json());
 
 // auth routes
 app.use("/api/auth", authRoutes);
+
+// app.get("/", (req, res) => {
+//   root route
+//   res.send("Server is running!");
+// });
+
 
 app.listen(PORT, () => {
   connectToMongoDB();
