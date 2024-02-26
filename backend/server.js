b322ddbaa9deddf650d262ddc49a0b57a3331df8
Miraj Asraf <mirajasraf786@gmail.com>
2024-02-26 21:27:30 +0530

    Created message route, message controller and protect route middleware

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 import connectToMongoDB from "./database/connectToMongoDB.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -11,8 +13,13 @@ dotenv.config();
 // to parse the incoming requests with JSON payloads (from req.body)
 app.use(express.json());
 
+app.use(cookieParser());
+
 // auth routes
 app.use("/api/auth", authRoutes);
+
+// message routes
+app.use("/api/messages", messageRoutes);
 
 // app.get("/", (req, res) => {
 //   root route
