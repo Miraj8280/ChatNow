03f4d335529a9541d2baa8fb5b39a837903b6a3b
Miraj Asraf <mirajasraf786@gmail.com>
2024-03-01 23:04:49 +0530

    Real-time online status feature added

--- frontend/src/components/sidebar/Conversation.jsx
+++ frontend/src/components/sidebar/Conversation.jsx
@@ -1,8 +1,13 @@
+import { useSocketContext } from "../../contexts/SocketContext";
 import useConversation from "../../zustand/useConversation";
 
 const Conversation = ({ conversation, emoji, lastIndex }) => {
   const { selectedConversation, setSelectedConversation } = useConversation();
+
   const isSelected = selectedConversation?._id === conversation._id;
+  const { onlineUsers } = useSocketContext();
+  const isOnline = onlineUsers.includes(conversation._id);
+
   return (
     <>
       <div
@@ -11,7 +16,7 @@ const Conversation = ({ conversation, emoji, lastIndex }) => {
       `}
         onClick={() => setSelectedConversation(conversation)}
       >
-        <div className="avatar online">
+        <div className={`avatar ${isOnline ? "online" : ""}`} >
           {" "}
           <div className="w-12 rounded-full">
             {" "}
